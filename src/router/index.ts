410d61e25beebf609c0d4fb22860b609c26d7fdc
Bayeeaa <2401428618@qq.com>
2024-09-16 14:21:42 +0800

    修改首页导航

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,7 @@ const router = createRouter({
     history:createWebHistory(),
     routes:[
         {
-           path:'/' ,
+           path:'/cc_shop' ,
            component:Home,
         },
         {
